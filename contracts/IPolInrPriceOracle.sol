25391a03b046c5a6e31d0fc75b3286395871721d
vidhanmangla <101626397+vidhanmangla@users.noreply.github.com>
2025-07-16 14:51:36 +0530

    chore: solc version bump to 0.8.30

--- contracts/IPolInrPriceOracle.sol
+++ contracts/IPolInrPriceOracle.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-
-pragma solidity ^0.8.29;
+pragma solidity 0.8.30;
 
 interface IPolInrPriceOracle {
     function getLatestPolInrPrice(address _payer) external returns (uint256 requestId);
--- contracts/IUsdtInrPriceOracle.sol
+++ contracts/IUsdtInrPriceOracle.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-
-pragma solidity ^0.8.29;
+pragma solidity 0.8.30;
 
 interface IUsdtInrPriceOracle {
     function getLatestUsdtInrPrice(address _payer) external returns (uint256 requestId);
